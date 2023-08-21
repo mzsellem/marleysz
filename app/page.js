69e9d9b7d194f6aa4f0e00fc77a0d1e7c987d5c1
mzsellem <marleysellem@gmail.com>
2023-08-21 13:37:03 -0400

    style home page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,19 @@
-import Navbar from "./navbar/page"
+import Navbar from "./navbar/page";
+import "../public/poppy2.png"
 
 export default function Home() {
   return (
-    <>
+    <main>
       <Navbar/>
-      <h1>Home Page</h1>
-    </>
+      <div className="flex flex-row items-center justify-center h-screen border-4 border-black rounded-3xl m-12 mt-5">
+        <div className="flex-col justify-center items-center w-3/5">
+          <h1 className="font-extralight text-7xl p-2 underline decoration-4 underline-offset-8 flex justify-center">MARLEY SELLEM</h1>
+          <h1 className="text-2xl font-thin flex justify-center pt-2">FULL STACK SOFTWARE ENGINEER</h1>
+        </div>
+        <div className="flex items-center w-2/5">
+          <img src="/poppy2.png" alt="poppy in a field" className="w-5/6"/>
+        </div>
+      </div>
+    </main>
   )
 }
