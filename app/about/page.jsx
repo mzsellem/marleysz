49bbc3731ff298b534b7bc93cc9d5d19c23d0c88
mzsellem <marleysellem@gmail.com>
2023-08-21 13:15:06 -0400

    test 2

---
+++ app/about/page.jsx
@@ -0,0 +1,10 @@
+import Navbar from "../Navbar/page";
+
+export default function About() {
+   return (
+      <>
+         <Navbar />
+         <h1>About Page</h1>
+      </>
+   );
+}
---
+++ app/navbar/page.jsx
@@ -0,0 +1,15 @@
+export default function Navbar() {
+   return (
+      <>
+         <nav className="flex justify-center">
+            <div>
+               <a href="/">Home</a>
+               <a href="/about">About</a>
+               <a href="/skills">Skills</a>
+               <a href="/mywork">My Work</a>
+               <a href="/contact">Contact</a>
+            </div>
+         </nav>
+      </>
+   );
+}
