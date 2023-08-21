d2e7191f6de377543e13d2a98a567920686cbc58
mzsellem <marleysellem@gmail.com>
2023-08-21 13:41:12 -0400

    style nav bar

--- app/navbar/page.jsx
+++ app/navbar/page.jsx
@@ -1,13 +1,35 @@
 export default function Navbar() {
    return (
       <>
-         <nav className="flex justify-center">
+         <nav className="flex justify-center mt-4">
             <div>
-               <a href="/">Home</a>
-               <a href="/about">About</a>
-               <a href="/skills">Skills</a>
-               <a href="/mywork">My Work</a>
-               <a href="/contact">Contact</a>
+               <a className="hover:underline mx-4 underline-offset-4" href="/">
+                  Home
+               </a>
+               <a
+                  className="hover:underline mx-4 underline-offset-4"
+                  href="/about"
+               >
+                  About
+               </a>
+               <a
+                  className="hover:underline mx-4 underline-offset-4"
+                  href="/skills"
+               >
+                  Skills
+               </a>
+               <a
+                  className="hover:underline mx-4 underline-offset-4"
+                  href="/mywork"
+               >
+                  My Work
+               </a>
+               <a
+                  className="hover:underline mx-4 underline-offset-4"
+                  href="/contact"
+               >
+                  Contact
+               </a>
             </div>
          </nav>
       </>
