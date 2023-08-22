49fb8fc10e52c10fdbad6a521fd52d0458110e2c
mzsellem <marleysellem@gmail.com>
2023-08-22 12:42:09 -0400

    style mywork done, add links

--- app/mywork/page.jsx
+++ app/mywork/page.jsx
@@ -7,38 +7,70 @@ export default function MyWork() {
          <div className="flex flex-row h-full border-4 border-black rounded-3xl m-12 mt-5 p-0  bg-custom">
             <div className="flex w-1/2 h-full">
                <div className="flex flex-col w-full h-1/2">
-                  <div className="flex justify-center items-center h-full bg-custom rounded-3xl">
-                     <div className="border w-1/2 p-4 text-5xl">
+                  <div className="flex justify-center items-center h-full bg-custom-tan rounded-tl-3xl p-11">
+                     <div className="w-3/4 p-4 text-5xl">
                         Find the Flag{" "}
                         <p className="text-xl">
                            Browser-based card matching game
                         </p>
+                        <button className="bg-black text-white text-xl rounded-lg p-2">
+                           {" "}
+                           Play
+                        </button>
+                        <button className="bg-black text-white text-xl rounded-lg p-2 ml-4">
+                           {" "}
+                           Github Repo
+                        </button>
                      </div>
                   </div>
-                  <div className="flex justify-center items-center h-full bg-custom-tan rounded-bl-2xl">
-                     <div className="border w-1/2 p-4 text-5xl">
+                  <div className="flex justify-center items-center h-full bg-custom-dark-tan rounded-bl-3xl p-14">
+                     <div className="w-3/4 p-4 text-5xl">
                         Sellem Sitters
                         <p className="text-xl">Cat-sitting tracker app</p>
+                        <button className="bg-black text-white text-xl rounded-lg p-2">
+                           {" "}
+                           Play
+                        </button>
+                        <button className="bg-black text-white text-xl rounded-lg p-2 ml-4">
+                           {" "}
+                           Github Repo
+                        </button>
                      </div>
                   </div>
                </div>
             </div>
             <div className="flex w-1/2 h-full">
                <div className="flex flex-col w-full h-1/2">
-                  <div className="flex justify-center items-center h-full bg-custom-red rounded-tr-2xl">
-                     <div className="border w-1/2 p-4 text-5xl">
+                  <div className="flex justify-center items-center h-full bg-custom-red rounded-tr-2xl p-8">
+                     <div className="w-3/4 p-4 text-5xl">
                         Soundify
                         <p className="text-xl">
                            Music streaming app with playlist capabilities
                         </p>
+                        <button className="bg-black text-white text-xl rounded-lg p-2">
+                           {" "}
+                           Play
+                        </button>
+                        <button className="bg-black text-white text-xl rounded-lg p-2 ml-4">
+                           {" "}
+                           Github Repo
+                        </button>
                      </div>
                   </div>
-                  <div className="flex justify-center items-center h-full">
-                     <div className="border w-1/2 p-4 text-5xl">
+                  <div className="flex justify-center items-center h-full p-8">
+                     <div className="w-3/4 p-4 text-5xl">
                         Lamadoro{" "}
                         <p className="text-xl">
                            Music streaming app with playlist capabilities
                         </p>
+                        <button className="bg-black text-white text-xl rounded-lg p-2">
+                           {" "}
+                           Play
+                        </button>
+                        <button className="bg-black text-white text-xl rounded-lg p-2 ml-4">
+                           {" "}
+                           Github Repo
+                        </button>
                      </div>
                   </div>
                </div>
