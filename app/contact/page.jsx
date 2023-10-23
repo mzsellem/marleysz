5c76ffed4331e089fa4e6d6f4b827ff8ac1d1d9b
mzsellem <marleysellem@gmail.com>
2023-10-23 11:57:17 -0400

    remove contact colors

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,9 +1,9 @@
 export default function Contact() {
    return (
       <>
-         <div className="flex flex-col h-full border-4 border-black rounded-3xl m-12 mt-5">
-            <div className="flex justify-center flex-col flex-grow-0.75 bg-custom-dark-tan rounded-tl-2xl rounded-tr-2xl p-20 -space-y-12">
-               <p className="flex justify-center text-6xl font-semibold p-20">
+         <div className="flex flex-col h-full border-4 border-black rounded-3xl m-12 space-y-6 p-20">
+            <div className="flex justify-center flex-col space-y-6">
+               <p className="flex justify-center text-6xl font-semibold">
                   Let's Chat
                </p>
                <p className="flex justify-center text-4xl font-fine">
@@ -11,7 +11,7 @@ export default function Contact() {
                   marleysellem@gmail.com
                </p>
             </div>
-            <div className="flex justify-center space-x-6 bg-custom-dark-tan pb-2">
+            <div className="flex justify-center space-x-6 pb-2">
                <a href="https://github.com/mzsellem-GA">
                   <img
                      src="/github-mark.png"
@@ -31,10 +31,6 @@ export default function Contact() {
                      height="50"
                   />
                </a>
-            </div>
-            <div className="flex flex-row flex-grow-0.25">
-               <div className="flex w-5/6 bg-custom-tan rounded-bl-2xl justify-center items-center text-3xl font-medium"></div>
-               <div className="flex w-1/6 bg-custom-red rounded-br-2xl p-20"></div>
             </div>
          </div>
       </>
