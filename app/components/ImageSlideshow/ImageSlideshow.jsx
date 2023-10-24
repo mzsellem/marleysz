ec87f3d79e07cd7702a2b106ef5fd108cbcaf234
mzsellem <marleysellem@gmail.com>
2023-10-24 15:43:09 -0400

    slideshow for my work

---
+++ app/components/ImageSlideshow/ImageSlideshow.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+export default function ImageSlideshow({ images }) {
+   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+   const nextImage = () => {
+      if (currentImageIndex < images.length - 1) {
+         setCurrentImageIndex(currentImageIndex + 1);
+      }
+   };
+
+   const prevImage = () => {
+      if (currentImageIndex > 0) {
+         setCurrentImageIndex(currentImageIndex - 1);
+      }
+   };
+
+   return (
+      <div className="relative">
+         <img
+            src={images[currentImageIndex]}
+            alt="Slideshow"
+            className="w-full h-auto"
+         />
+         <button
+            onClick={prevImage}
+            className="absolute left-0 top-1/2 transform -translate-y-1/2 p-2 bg-gray-800 text-white rounded-full"
+         >
+            &lt;
+         </button>
+         <button
+            onClick={nextImage}
+            className="absolute right-0 top-1/2 transform -translate-y-1/2 p-2 bg-gray-800 text-white rounded-full"
+         >
+            &gt;
+         </button>
+      </div>
+   );
+}
--- app/mywork/page.jsx
+++ app/mywork/page.jsx
@@ -1,13 +1,40 @@
 "use client";
 import React from "react";
 import "../../public/logo.png";
+import ImageSlideshow from "../components/ImageSlideshow/ImageSlideshow";
+import "../../public/account.png";
+import "../../public/ai-chatbot.png";
 
 export default function MyWork() {
+   const images = [
+      "login.png",
+      "account.png",
+      "ready-for-challenge.png",
+      "ai-chatbot.png",
+      "analytics.png",
+      "feedback.png",
+      "control-panel.png",
+   ];
    return (
       <>
-         <div>
-            <h1>hello</h1>
-         </div>
+         <ul>Projects</ul>
+         <li>
+            <div className="flex flex-col">
+               <h1>AI Chatbot </h1>
+               <div className="image-slideshow border flex justify-center">
+                  <ImageSlideshow images={images} />
+               </div>
+               <div className="flex flex-row">
+                  Lorem ipsum is placeholder text commonly used in the graphic,
+                  print, and publishing industries for previewing layouts and
+                  visual mockups.
+               </div>
+            </div>
+         </li>
+         <li></li>
+         <li></li>
+         <li></li>
+         <li></li>
       </>
    );
 }
