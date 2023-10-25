8a0eba78261a49c04af62d7765d1c9a4329551df
mzsellem <marleysellem@gmail.com>
2023-10-25 14:49:54 -0400

    intro almost mobile friendly

--- app/intro/page.jsx
+++ app/intro/page.jsx
@@ -1,20 +1,20 @@
 export default function Intro() {
    return (
       <>
-         <div className="flex flex-col md:flex-row md:items-start md:h-full border-4 border-black rounded-3xl p-8">
-            <div className="flex flex-col justify-start items-start w-3/5">
-               <h1 className="text-6xl font-extralight flex justify-start md:text-7xl p-2 underline decoration-4 underline-offset-8">
+         <div className="flex flex-col md:h-full border-4 rounded-3xl p-8">
+            <div className="flex flex-col justify-center text-center">
+               <h1 className="text-6xl font-extralight md:text-8xl p-2 underline decoration-4 underline-offset-8">
                   MARLEY SELLEM
                </h1>
             </div>
-            <div className="mt-4 md:mt-0 flex justify-end">
-               <h1 className="text-xl md:text-2xl font-thin flex justify-start pt-2 ml-2">
+            <div className="mt-6 flex md:flex-col items-center justify-center ml-6">
+               <h1 className="text-xl md:text-4xl font-thin text-start mr-2 mb-6">
                   FULL STACK SOFTWARE ENGINEER
                </h1>
                <img
                   src="/poppy2.png"
                   alt="poppy in a field"
-                  className="w-2/5 md:w-4/6"
+                  className="w-2/5 md:w-3/5"
                />
             </div>
          </div>
