bade14df378675f189696cc6611d9c10de09cda9
mzsellem <marleysellem@gmail.com>
2023-10-25 14:03:57 -0400

    working on home page mobile

--- app/intro/page.jsx
+++ app/intro/page.jsx
@@ -1,20 +1,20 @@
 export default function Intro() {
    return (
       <>
-         <div className="flex flex-row items-center justify-center h-full border-4 border-black rounded-3xl m-12 p-20">
-            <div className="flex-col justify-center items-center w-3/5">
-               <h1 className="font-extralight text-7xl p-2 underline decoration-4 underline-offset-8 flex justify-center">
+         <div className="flex flex-col md:flex-row md:items-start md:h-full border-4 border-black rounded-3xl p-8">
+            <div className="flex flex-col justify-start items-start w-3/5">
+               <h1 className="text-6xl font-extralight flex justify-start md:text-7xl p-2 underline decoration-4 underline-offset-8">
                   MARLEY SELLEM
                </h1>
-               <h1 className="text-2xl font-thin flex justify-center pt-2 ml-2">
+            </div>
+            <div className="mt-4 md:mt-0 flex justify-end">
+               <h1 className="text-xl md:text-2xl font-thin flex justify-start pt-2 ml-2">
                   FULL STACK SOFTWARE ENGINEER
                </h1>
-            </div>
-            <div className="flex items-center justify-center w-2/5">
                <img
                   src="/poppy2.png"
                   alt="poppy in a field"
-                  className="w-5/6"
+                  className="w-2/5 md:w-4/6"
                />
             </div>
          </div>
