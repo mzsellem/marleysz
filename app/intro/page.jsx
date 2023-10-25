ca31e52f8695681074ee6d6d179d045845d1da85
mzsellem <marleysellem@gmail.com>
2023-10-25 11:33:43 -0400

    intro page: web

--- app/intro/page.jsx
+++ app/intro/page.jsx
@@ -1,12 +1,12 @@
 export default function Intro() {
    return (
       <>
-         <div className="flex flex-row items-center justify-center h-full border-4 border-black rounded-3xl m-12 mt-5 p-20">
+         <div className="flex flex-row items-center justify-center h-full border-4 border-black rounded-3xl m-12 p-20">
             <div className="flex-col justify-center items-center w-3/5">
                <h1 className="font-extralight text-7xl p-2 underline decoration-4 underline-offset-8 flex justify-center">
                   MARLEY SELLEM
                </h1>
-               <h1 className="text-2xl font-thin flex justify-center pt-2">
+               <h1 className="text-2xl font-thin flex justify-center pt-2 ml-2">
                   FULL STACK SOFTWARE ENGINEER
                </h1>
             </div>
