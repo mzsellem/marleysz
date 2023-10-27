f1b1408518918424e138f50f6e4034e44e124a79
mzsellem <marleysellem@gmail.com>
2023-10-27 16:45:34 -0400

    work on skills

--- app/skills/page.jsx
+++ app/skills/page.jsx
@@ -1,42 +1,72 @@
 export default function Skills() {
+   const skillCats = [
+      {
+         category: "Languages",
+         list: ["HTML", "CSS", "Python", "EJS", "JavaScript"],
+      },
+      {
+         category: "Frameworks/Libraries",
+         list: ["Express.js", "Node.js", "React.js", "Django", "Mongoose"],
+      },
+      { category: "Databases", list: ["MongoDB", "PostgreSQL", "Supabase"] },
+      {
+         category: "Concepts/Patterns",
+         list: ["REST API", "Object Oriented Programming", "Agile", "Scrum"],
+      },
+      {
+         category: "Auth",
+         list: ["Google OAuth", "Passport.js", "Supabase Magic Link"],
+      },
+      {
+         category: "Other",
+         list: [
+            "Git",
+            "Github",
+            "Trello",
+            "Fly.io",
+            "Bootstrap",
+            "JSON",
+            "Tailwind CSS",
+            "TailWind UI",
+            "Resend",
+            "Vercel",
+            "Vercel AI SDK",
+         ],
+      },
+   ];
+
    return (
       <>
-         <div className="flex flex-col items-center justify-center h-full border-4 border-black rounded-3xl m-12 mt-5 p-8">
-            <div className="w-1/2">
-               <div className="text-5xl flex items-center mb-6">Skillset</div>
-               <div>
-                  <div className="text-xl mb-2">
-                     <span className="font-bold">Languages:</span> HTML,
-                     JavaScript, CSS, Python, EJS
-                  </div>
-                  <div className="text-xl mb-2">
-                     <span className="font-bold">Frameworks/Libraries:</span>{" "}
-                     Express.js, Node.js, React.js, Django, Mongoose
-                  </div>
-                  <div className="text-xl mb-2">
-                     <span className="font-bold">Databases:</span> MongoDB,
-                     PostgreSQL, Supabase
-                  </div>
-                  <div className="text-xl mb-2">
-                     <span className="font-bold">Concepts/Patterns:</span> REST,
-                     Object Oriented Programming, Agile, Scrum
-                  </div>
-                  <div className="text-xl mb-2">
-                     <span className="font-bold">Auth:</span> Google OAuth,
-                     Passport.js, Magic Link
-                  </div>
-                  <div className="text-xl mb-6">
-                     <span className="font-bold">Other:</span> Git, Github,
-                     Trello, Fly.io, Bootstrap, JSON, Tailwind, TailWind UI,
-                     Resend, Vercel, and Vercel AI SDK
+         <div className="flex flex-col justify-center h-full border-4 border-black rounded-3xl">
+            <div className="w-full p-6 overflow-x-auto">
+               <div className="text-5xl mb-6 mt-6">Skillset</div>
+               <div className="flex flex-row space-x-12 border">
+                  <div className="text-xl flex space-x-8 m-4">
+                     {skillCats.map((obj) => {
+                        return (
+                           <div key={obj.category}>
+                              <h1 className="font-bold">{obj.category}</h1>
+                              <ul className="border">
+                                 {obj.list.map((item) => (
+                                    <li
+                                       key={item}
+                                       className="list-inside list-disc"
+                                    >
+                                       {item}
+                                    </li>
+                                 ))}
+                              </ul>
+                           </div>
+                        );
+                     })}
                   </div>
                </div>
-               <div className="text-3xl mb-4">
+               <div className="w-full text-3xl border">
                   <span className="font-bold">Bonus</span>
-               </div>
-               <div className="text-xl">
-                  <span className="font-bold">Spoken Languages:</span> French
-                  (native) and Japanese
+                  <div className="text-xl">
+                     <span className="flex font-bold">Spoken Languages:</span>{" "}
+                     French (native) and Japanese
+                  </div>
                </div>
             </div>
          </div>
