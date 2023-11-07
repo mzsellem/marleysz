8231f8557e2104e033a9595bd5776e2f1c183a93
mzsellem <marleysellem@gmail.com>
2023-11-07 13:51:30 -0500

    transparency light on web for all sections

--- app/page.js
+++ app/page.js
@@ -42,13 +42,13 @@ export default function Home() {
             <div ref={sectionRefs.section2} className="section bg-blue-400/50 md:bg-blue-400/25 rounded-3xl my-2">
                 <Projects />
             </div>
-            <div ref={sectionRefs.section3} className="section bg-blue-400/50 rounded-3xl my-2">
+            <div ref={sectionRefs.section3} className="section bg-blue-400/50 md:bg-blue-400/25 rounded-3xl my-2">
                 <Skills />
             </div>
-            <div ref={sectionRefs.section4} className="section bg-blue-400/50 rounded-3xl my-2">
+            <div ref={sectionRefs.section4} className="section bg-blue-400/50 md:bg-blue-400/25 rounded-3xl my-2">
                 <About />
             </div>
-            <div ref={sectionRefs.section5} className="section bg-blue-400/50 rounded-3xl my-2">
+            <div ref={sectionRefs.section5} className="section bg-blue-400/50 md:bg-blue-400/25 rounded-3xl my-2">
                 <Contact />
           </div>
         </div>
