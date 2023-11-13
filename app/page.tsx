d04b2c2aa290305c760fac564787b7f5d93d16c8
mzsellem <marleysellem@gmail.com>
2023-11-13 11:20:34 -0500

    restructure scrolling on home

--- app/page.tsx
+++ app/page.tsx
@@ -7,35 +7,23 @@ import About from "./about/page";
 import Intro from "./intro/page";
 import "../public/poppy2.png"
 
-// Define an enum for section names
-enum SectionNames {
-  Section1 = "section1",
-  Section2 = "section2",
-  Section3 = "section3",
-  Section4 = "section4",
-  Section5 = "section5",
-}
-
-// Define a type for section refs
-type SectionRefs = Record<SectionNames, RefObject<HTMLDivElement>>;
-
 export default function Home() {
- 
-    // Use the SectionRefs type for sectionRefs
-    const sectionRefs: SectionRefs = {
-      section1: useRef<HTMLDivElement>(null),
-      section2: useRef<HTMLDivElement>(null),
-      section3: useRef<HTMLDivElement>(null),
-      section4: useRef<HTMLDivElement>(null),
-      section5: useRef<HTMLDivElement>(null),
-    };
-  
-    // Provide a type for the section parameter
-    const scrollToSection = (section: SectionNames) => {
-      if (sectionRefs[section] && sectionRefs[section].current) {
-        sectionRefs[section].current.scrollIntoView({ behavior: "smooth" });
-      }
-    };
+
+  const section1Ref = useRef(null);
+  const section2Ref = useRef(null);
+  const section3Ref = useRef(null);
+  const section4Ref = useRef(null);
+  const section5Ref = useRef(null);
+
+  const scrollToSectionWithOffset = (ref, offset) => {
+    if (ref.current) {
+      window.scrollTo({
+        top: ref.current.offsetTop - offset,
+        behavior: "smooth", // Optional, for smooth scrolling
+      });
+    }
+  };
+
 
   return (
     <>
@@ -43,30 +31,30 @@ export default function Home() {
     <div className="flex justify-center">
       <div className="p-4 font-mono text-black container rounded-2xl w-screen">
           <div className="flex justify-center space-x-4 text-sm md:text-base">
-            <button onClick={() => scrollToSection("section1")} className="hover:underline hover:decoration-poppyred hover:decoration-8">Intro</button>
-            <button onClick={() => scrollToSection("section2")} className="hover:underline hover:decoration-poppyred hover:decoration-8">Projects</button>
-            <button onClick={() => scrollToSection("section3")} className="hover:underline hover:decoration-poppyred hover:decoration-8">Skills</button>
-            <button onClick={() => scrollToSection("section4")} className="hover:underline hover:decoration-poppyred hover:decoration-8">About</button>
-            <button onClick={() => scrollToSection("section5")} className="hover:underline hover:decoration-poppyred hover:decoration-8">Contact</button>
+            <button onClick={() => scrollToSectionWithOffset(section1Ref, 20)} className="hover:underline hover:decoration-poppyred hover:decoration-8">Intro</button>
+            <button onClick={() => scrollToSectionWithOffset(section2Ref, 50)} className="hover:underline hover:decoration-poppyred hover:decoration-8">Projects</button>
+            <button onClick={() => scrollToSectionWithOffset(section3Ref, 50)} className="hover:underline hover:decoration-poppyred hover:decoration-8">Skills</button>
+            <button onClick={() => scrollToSectionWithOffset(section4Ref, 50)} className="hover:underline hover:decoration-poppyred hover:decoration-8">About</button>
+            <button onClick={() => scrollToSectionWithOffset(section5Ref, 50)} className="hover:underline hover:decoration-poppyred hover:decoration-8">Contact</button>
           </div>
       </div>
     </div>
     </div>
       <div>
-          <div ref={sectionRefs.section1} className="section h-screen">
+          <div ref={section1Ref} className="section h-screen">
               <Intro />
           </div>
           <div className="flex flex-col items-center m-4">
-            <div ref={sectionRefs.section2} className="section bg-blue-400/50 md:bg-blue-400/25 rounded-3xl my-2 mt-52 md:mt-44">
+            <div ref={section2Ref} className="section bg-blue-400/50 md:bg-blue-400/25 rounded-3xl my-2 mt-52 md:mt-44">
                 <Projects />
             </div>
-            <div ref={sectionRefs.section3} className="section bg-blue-400/50 md:bg-blue-400/25 rounded-3xl my-2">
+            <div ref={section3Ref} className="section bg-blue-400/50 md:bg-blue-400/25 rounded-3xl my-2">
                 <Skills />
             </div>
-            <div ref={sectionRefs.section4} className="section bg-blue-400/50 md:bg-blue-400/25 rounded-3xl my-2">
+            <div ref={section4Ref} className="section bg-blue-400/50 md:bg-blue-400/25 rounded-3xl my-2">
                 <About />
             </div>
-            <div ref={sectionRefs.section5} className="section bg-blue-400/50 md:bg-blue-400/25 rounded-3xl my-2">
+            <div ref={section5Ref} className="section bg-blue-400/50 md:bg-blue-400/25 rounded-3xl my-2">
                 <Contact />
           </div>
         </div>
