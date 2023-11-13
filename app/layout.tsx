239e2fe1764e814197587dffa259be300a8279d5
mzsellem <marleysellem@gmail.com>
2023-11-13 15:53:14 -0500

    upgrade next to 14.0.2, reformatted postcss

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { ReactNode } from "react"
+import React, { ReactNode } from "react"
 
 const inter = Inter({ subsets: ['latin'] })
 
