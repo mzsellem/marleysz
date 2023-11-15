c3d90a1b4837aed010b536f4231f27eef31f0a33
mzsellem <marleysellem@gmail.com>
2023-11-15 14:58:02 -0500

    better alignment for intro

--- app/intro/page.tsx
+++ app/intro/page.tsx
@@ -16,7 +16,7 @@ export default function Intro() {
          </div>
          <div className="flex flex-col md:h-full rounded-3xl p-8 mx-auto max-w-screen-md font-mono text-black">
             <div className="flex flex-col justify-center items-center text-center m-2">
-               <h1 className="text-6xl font-extralight md:text-8xl md:mt-40 m-2 underline decoration-4 underline-offset-8">
+               <h1 className="text-6xl font-extralight md:text-8xl md:mt-52 m-2 underline decoration-4 underline-offset-8">
                   MARLEY SELLEM
                </h1>
                <div className="text-xl md:mb-4 w-screen md:text-3xl mt-8 font-thin text-center">
