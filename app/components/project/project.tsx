1150d3d8a60bf642e3c3ebeb648a36d82bdc00de
mzsellem <marleysellem@gmail.com>
2024-04-02 14:54:51 -0400

    make images same size and remove unused css

--- app/components/project/project.tsx
+++ app/components/project/project.tsx
@@ -12,15 +12,15 @@ export type ProjectProps = {
 export default function Project({ image, contract, title, description, techLinks, projectLink }: ProjectProps) {
    return (
       <div className="flex flex-col items-center text-black">
-         <div>{title}</div>
-         <div className="flex format group">
+         <div className="mb-3">{title}</div>
+         <div className="flex border image-container">
             <a href={projectLink}>
-               <img className="flex items-center justify-center mt-4 transition-transform border-4 rounded-lg image-slideshow group-hover:scale-110 border-grey" src={image} alt={title} />
+               <img className="flex items-center justify-center mt-4 transition-transform border-4 rounded-lg project-image hover:scale-110 border-grey" src={image} alt={title} />
             </a>
          </div>
-         <div className="flex m-6 text-sm text-center">{description}</div>
+         <div className="flex m-5 text-sm text-center">{description}</div>
          {contract !== undefined && ( // Conditional rendering of contract
-            <div className="flex m-6 text-sm text-center">{contract}</div>
+            <div className="flex mb-4 text-sm text-center m">{contract}</div>
          )}
          <div className="flex flex-wrap items-center justify-center mb-4 -my-2 space-x-2">
             {techLinks.map((tech, index) => (
@@ -28,7 +28,7 @@ export default function Project({ image, contract, title, description, techLinks
                   key={index}
                   href={tech.link}
                >
-                  <div className="p-2 my-2 text-xs text-black border rounded-xl hover:bg-brightred hover:text-white">
+                  <div className="p-2 text-xs text-black border rounded-xl hover:bg-brightred hover:text-white">
                      {tech.name}
                   </div>
                </a>
