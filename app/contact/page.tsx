1127613e145578effc61c2ed89444564b782958f
mzsellem <marleysellem@gmail.com>
2024-05-22 17:35:30 -0400

    reformat contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,39 +3,36 @@ import React from "react"
 export default function Contact() {
    return (
       <>
-         <div className="flex flex-col w-3/4 p-6 font-mono text-black h-[210px] sm:h-[220px] max-w-[800px] sm:w-[700px]">
-            <div className="flex flex-col justify-center mb-6 space-y-6">
-               <p className="flex justify-center text-3xl font-semibold sm:text-5xl">
-                  Let's Chat
-               </p>
-               <p className="flex justify-center text-xl font-fine">
+         <div className="flex flex-col font-mono text-black sm:mb-4 sm:w-[700px]">
+            <div className="flex flex-col justify-center">
+               <p className="flex justify-center m-4 text-xl font-fine">
                   {" "}
                   marleysellem@gmail.com
                </p>
-            </div>
-            <div className="flex justify-center pb-2 space-x-6">
-               <a 
-                  href="https://github.com/mzsellem-GA"
-                  className="mt-1 glow-on-hover"
-               >
-                  <img
-                     src="/github-mark.png"
-                     alt="github logo"
-                     width="40"
-                     height="40"
-                  />
-               </a>
-               <a
-                  href="https://www.linkedin.com/in/marley-sellem/"
-                  className="mt-2 glow-on-hover"
-               >
-                  <img
-                     src="/li-logo.png"
-                     alt="linkedin logo"
-                     width="40"
-                     height="40"
-                  />
-               </a>
+               <div className="flex justify-center pb-2 space-x-6">
+                  <a 
+                     href="https://github.com/mzsellem-GA"
+                     className="mt-1"
+                  >
+                     <img
+                        src="/github-mark.png"
+                        alt="github logo"
+                        width="40"
+                        height="40"
+                     />
+                  </a>
+                  <a
+                     href="https://www.linkedin.com/in/marley-sellem/"
+                     className="mt-2"
+                  >
+                     <img
+                        src="/li-logo.png"
+                        alt="linkedin logo"
+                        width="40"
+                        height="40"
+                     />
+                  </a>
+               </div>
             </div>
          </div>
       </>
