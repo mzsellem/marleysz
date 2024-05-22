cdba176c52cada3432e92f9e8a4a5ab3d7c2a479
mzsellem <marleysellem@gmail.com>
2024-05-22 17:24:49 -0400

    reformat about me

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,7 @@ import "../../public/marley.png";
 export default function About() {
    return (
       <>
-         <div className="flex flex-col items-center justify-start w-full h-full p-2 mx-auto mt-6 font-mono text-black">
+         <div className="flex flex-col items-center w-full h-full mt-2 font-mono text-black">
             <div className="flex justify-center w-3/4 sm:w-1/2 sm:justify-center">
                <img
                   src="/marley.png"
@@ -12,30 +12,14 @@ export default function About() {
                   className="w-full h-auto border-4 rounded-lg sm:w-1/2"
                />
             </div>
-            <div className="w-full mt-6 text-center sm:text-center h-[900px] sm:h-[405px] max-w-[800px] sm:w-[700px]">
-               <div className="mb-4 font-mono text-4xl md:text-center sm:text-4xl sm:mb-4">
-                  About Me
-               </div>
-               <p className="mb-4 ml-8 mr-8 sm:m-0 sm:mb-6">
-               As a software engineer with a strong foundation in health 
-               sciences, I excel in delivering meticulous, user-centered
-               solutions. My dedication to client and user satisfaction
-               is rooted in my detailed-oriented approach and unwavering
-               commitment to excellence. 
-               </p>
-               <p className="mb-4 ml-8 mr-8 sm:m-0 sm:mb-6">
-                  Growing up as a third culture kid, I bring a unique, global
-                  perspective and adaptability, enabling me to thrive in 
-                  diverse, collaborative environments. I am eager to apply my
-                  multifaceted skills and experiences to create impactful and
-                  innovative software solutions that 
-                  meet and exceed user expectations.               
-               </p>
-               <p className="mb-4 ml-8 mr-8 sm:m-0 sm:mb-6">
-                  With an innate curiosity and a strong drive to learn, I am
-                  confident that my transition into software development will
-                  enable me to make a meaningful impact and thrive in this
-                  field.
+            <div className="w-full sm:mt-4 text-center sm:text-center sm:w-[700px]">
+               <p className="p-6 mb-4 sm:p-0 sm:mb-6">
+               As a detail-oriented junior software engineer, I am dedicated to
+               crafting seamless user experiences that cater to diverse needs. 
+               My multicultural background enriches my approach to problem-solving,
+               allowing me to create inclusive and effective solutions. I am eager 
+               to bring my enthusiasm and skills to a forward-thinking team, where 
+               I can continue to grow and make a meaningful impact. 
                </p>
             </div>
          </div>
