d441283cb9858c83c0f44348535d01216b0d4828
mzsellem <marleysellem@gmail.com>
2024-05-22 15:33:19 -0400

    simplify footer styling

--- app/footer/footer.tsx
+++ app/footer/footer.tsx
@@ -2,10 +2,8 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <div className='flex justify-center p-2'>
-            <footer className="flex items-center justify-center font-bold">
-                <div className='flex justify-center'>&copy; 2024 Marleysz</div>
-            </footer>
-        </div>
+        <footer className="flex justify-center font-bold">
+            <p className=''>&copy; 2024 Marleysz</p>
+        </footer>
     )
 }
