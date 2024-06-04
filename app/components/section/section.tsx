12ee68fda89bcd7a2958688e1b7e211786f5a49d
mzsellem <marleysellem@gmail.com>
2024-06-04 11:07:22 -0400

    opacity test on pixel

--- app/components/section/section.tsx
+++ app/components/section/section.tsx
@@ -8,7 +8,7 @@ export type SectionProps = {
 export default function Section({title, component}: SectionProps) {   
    return (
       <div className="flex flex-col m-4 sm:items-center">
-         <div className="flex flex-col items-center justify-center w-full p-4 bg-blue-400/50 sm:bg-blue-400/25 rounded-3xl sm:w-3/4">
+         <div className="flex flex-col items-center justify-center w-full p-4 bg-blue-400/75 sm:bg-blue-400/25 rounded-3xl sm:w-3/4">
             <h1 className="mt-6 mb-2 font-mono text-4xl">{title}</h1>
             {component} 
          </div>              
