197360c9279e9174ec71dbf66e2e57f5e5c2f9d9
mzsellem <marleysellem@gmail.com>
2024-06-13 10:34:39 -0400

    edit about me

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -14,12 +14,13 @@ export default function About() {
             </div>
             <div className="w-full sm:mt-4 text-center sm:text-center sm:w-[700px]">
                <p className="p-6 mb-4 sm:p-0 sm:mb-6">
-               As a detail-oriented software engineer, I am dedicated to
-               crafting seamless user experiences that cater to diverse needs. 
-               My multicultural background enriches my approach to problem-solving,
-               allowing me to create inclusive and effective solutions. I am eager 
-               to bring my enthusiasm and skills to a forward-thinking team, where 
-               I can continue to grow and make a meaningful impact. 
+               Hi, I'm Marley Sellem, a dynamic full-stack software engineer deeply passionate about front-end 
+               design. Drawing from a diverse cultural background and prior experience in the medical field, I 
+               bring a unique blend of precision and creativity to every project. My journey in healthcare has 
+               sharpened my ability to navigate complex challenges with clarity and empathy. With a keen eye for 
+               detail and a client-centric approach, I ensure each solution is not only technically robust but also 
+               aesthetically compelling. I am dedicated to crafting seamless user experiences that exceed expectations, 
+               making me a valuable addition to any innovative team.
                </p>
             </div>
          </div>
